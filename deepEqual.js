34aa9299a167f79998c9e1695024f5f7739fcf6b
Manuel Santiago <manuel_sm223@hotmail.com>
2021-01-19 20:54:00 -0600

    deepEqual

---
+++ deepEqual.js
@@ -0,0 +1,46 @@
+function deepEqual(a, b) {
+    if(a === b){
+        return true;
+    }
+
+    var keys1 = Object.keys(a);
+    var keys2 = Object.keys(b);
+
+    var values1 = Object.values(a);
+    var values2 = Object.values(b);
+
+     if(keys1.length == keys2.length ){
+        for(var i = 0; i < keys1.length; i++){
+            if(typeof(a[keys1[i]]) == 'object' && typeof(b[keys1[i]]) == 'object'){
+                deepEqual(a[keys1[i]], b[keys1[i]]);
+            }
+            if(keys1[i] !== keys2[i] && values1[i] !== values2[i]){
+                return false;
+            }
+        }
+        return true;
+    }else return false; 
+
+  }
+
+  console.log(deepEqual(john, john2));
+  
+  var john = {
+    firstName: 'John',
+    lastName: 'Doe',
+  }
+
+  var john2 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    address: {street: 'qweeqw', number: 4}
+  }
+  
+
+
+  console.log('Test 1:', deepEqual(1, 1)) // true
+  console.log('Test 2:', deepEqual(1, '1')) // false
+  console.log('Test 3:', deepEqual(john, john)) // true
+  console.log('Test 4:', deepEqual(john, { firstName: 'John', lastName: 'Doe' })) // true
+  console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false 
+  console.log('Test 6:', deepEqual(john, john2))  
